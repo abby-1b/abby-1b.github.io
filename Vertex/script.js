e2428d0a46caf7137efa8e9aaf6068142ea86ca8
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2020-06-21 23:47:53 -0400

    Create script.js

---
+++ Vertex/script.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById("screen");
+var ctx = canvas.getContext("2d");
+
+var width  = canvas.width;
+var height = canvas.height;
+
+var black = -16777216;
+var white = -1;
+
+var imageData = ctx.getImageData(0, 0, width, height);
+var buf = new ArrayBuffer(imageData.data.length);
+var buf8 = new Uint8ClampedArray(buf);
+var screen = new Uint32Array(buf);
+
+function clearS() {
+  buf8.fill("-1");
+}
+function drawS() {
+  imageData.data.set(buf8);
+  ctx.putImageData(imageData, 0, 0);
+}
+
+for (var x = 0; x < width / 8; x++) {
+  for (var y = 0; y < height / 8; y++) {
+    screen[(x * 8) + (y * width * 8)] = black;
+  }
+}
+drawS();
