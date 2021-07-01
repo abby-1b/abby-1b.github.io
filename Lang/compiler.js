9eb710a0398903606571178385269f34d2724275
CodeIGuess <codeiguess@gmail.com>
2021-07-01 19:37:41 -0400

    Multiple improvements, mostly functionality
     - Removed `arr` and `var` types
     - `using namespace std` ;)
     - Added string functions (lowercase, uppercase, integer conversion)
     - Support for arrays (actually vectors once compiled but oh well ¯\_(ツ)_/¯)
     - Functions!
     - Better error messages
    
    The compiler still produces some code that doesn't compile on g++, but the point is to always catch those errors eventually :)

--- Lang/compiler.js
+++ Lang/compiler.js
@@ -2,21 +2,21 @@
 
 let opTypes = "=+-*/<>"
 
-let varTypes = ["int", "str", "flt", "dbl", "arr", "fn", "var"]
+let varTypes = ["int", "str", "flt", "dbl", "fn", "def"]
 let formattedVarTypes = varTypes.map(e => e + "Type")
 
 let lTypesToCTypes = {
     "int": "int",
-    "str": "std::string",
+    "str": "string",
     "flt": "float",
     "dbl": "double",
-    "arr": "???",
     "fn":  "???",
-    "var": "???"
+    "def": "void"
 }
 
 let stats = {
-    "print": "_"
+    "print": "print",
+    "input": "input"
 }
 
 let defaultContent = {
@@ -41,31 +41,92 @@ let operationFunctions = {
     "<=": "leq",
     "=<": "leq",
     ">=": "meq",
-    "=>": "meq"
+    "=>": "meq",
+
+    "l": "String::length(%)",
+    "tint": "String::toint(%)",
+    "lower": "String::lower(%)",
+    "upper": "String::upper(%)"
 }
 
-let genCodeStart = `
+let genCodeLib = `
+// std::vector<int> v { 10, 20, 30 };
+
+// transform(v.begin(), v.end(), v.begin(), [](int &c){ return c * 2; });
+
+// for (int a : v) {
+//     print(a); print(" ");
+// }
+
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
 
-std::string _(std::string i) { std::cout << i; return i; }
-float       _(float i)       { std::cout << i; return i; }
+using namespace std;
 
-float       _sum(float a      , float b      ) { return a + b; }
-std::string _sum(std::string a, float b      ) { return a + std::to_string(b); }
-std::string _sum(float a      , std::string b) { return std::to_string(a) + b; }
-std::string _sum(std::string a, std::string b) { return a + b; }
+class String {
+    public:
+        static int toint(string a) {
+            try {
+                return stoi(a);
+            } catch (...) {
+                return 0;
+            }
+        }
+        static string lower(string a) {
+            transform(a.begin(), a.end(), a.begin(), [](unsigned char c){ return tolower(c); });
+            return a;
+        }
+        static string upper(string a) {
+            transform(a.begin(), a.end(), a.begin(), [](unsigned char c){ return toupper(c); });
+            return a;
+        }
+        static int length(string a) {
+            return a.length();
+        }
+};
 
-float       _mul(float a      , float b      ) { return a * b; }
-std::string _mul(std::string a, int b        ) { std::string r = ""; for (int c = 0; c < b; c++) { r += a; } return r; }
+class Vector {
+};
 
-int         _eql(float a      , float b      ) { return a == b; }
-int         _eql(std::string a, std::string b) { return a == b; }
-int         _lss(float a      , float b      ) { return a < b; }
-int         _lss(std::string a, std::string b) { return a.length() < b.length(); }
-int         _mrr(float a      , float b      ) { return a > b; }
-int         _mrr(std::string a, std::string b) { return a.length() > b.length(); }
+string print(string i) { cout << i; return i; }
+float  print(float i)  { cout << i; return i; }
 
+string input(string i) { cout << i; string _retstr; getline(cin, _retstr); return _retstr; }
+
+float  _sum(float a , float b ) { return a + b; }
+string _sum(string a, float b ) { return to_string(b) + a; }
+string _sum(float a , string b) { return to_string(a) + b; }
+string _sum(string a, string b) { return a + b; }
+
+float  _sub(float a , float b ) { return a - b; }
+// string _sub(string a, float b ) { return to_string(b) + a; }
+// string _sub(float a , string b) { return to_string(a) + b; }
+// string _sub(string a, string b) { return a + b; }
+
+float  _mul(float a , float b ) { return a * b; }
+string _mul(string a, int b   ) { string r = ""; for (int c = 0; c < b; c++) { r += a; } return r; }
+
+int _eql(float a , float b ) { return a == b; }
+int _eql(string a, string b) { return a == b; }
+
+int _lss(float a , float b ) { return a < b; }
+int _lss(string a, string b) { return a.length() < b.length(); }
+
+int _leq(float a , float b ) { return a <= b; }
+int _meq(float a , float b ) { return a >= b; }
+
+int _mrr(float a , float b ) { return a > b; }
+int _mrr(string a, string b) { return a.length() > b.length(); }
+
+`
+
+let functions = ""
+let variableCode = ""
+
+let genCodeStart = `
 int main() {
 `
 
@@ -77,9 +138,11 @@ let genCodeEnd = `
 
 let controlFlow = ["if", "while", "for", "scan"]
 
+let lastVariableType = ""
+
 if (process.argv.length == 2) {
     console.error("usage: compiler [file name]")
-    process.exit()
+    process.exit(1)
 }
 
 const fs = require("fs")
@@ -94,12 +157,12 @@ String.prototype.any = function(c) { return this.includes(c) }
 
 function error(message) {
     console.error(`\x1b[31mERROR at ${arguments.callee.caller.name}: ${message}`)
-    process.exit()
+    process.exit(1)
 }
 
 function save(code, name) {
-    fs.writeFile(name + ".cpp", code, "utf8", _=>{
-        exec(`g++ ${name}.cpp ${process.argv.splice(3).join(" ")}`, (err) => {
+    fs.writeFile(name + ".cpp", code, "utf8", function fileWrite(){
+        exec(`g++ -std=c++11 -stdlib=libc++ ${name}.cpp ${process.argv.splice(3).join(" ")}`, function compile(err) {
             if (err) {
                 error(err)
             }
@@ -110,14 +173,16 @@ function save(code, name) {
 function compile(program) {
     let tokens   = tokenize  (program)
     let ast      = parse     (tokens)  // This is mutated with modify. Oh no.
-                   modify    (ast)
-                   operations(ast)
-                   variables (ast)
-                   control   (ast)
+    modify    (ast)
+    operations(ast)
+    variables (ast)
     console.log(util.inspect(ast, false, null, true))
+    control   (ast)
     let code     = generate(ast)
-            code = indent(code, 1)
-    code = genCodeStart + code + genCodeEnd
+    code      = indent(code, 1)
+    functions = indent(functions, 0)
+    variableCode = indent(variableCode, 0)
+    code = genCodeLib + variableCode + functions + genCodeStart + code + genCodeEnd
     return code
 }
 
@@ -135,6 +200,11 @@ function tokenize(program) {
             tokens.push({
                 type: "sep",
                 content: ','
+            })
+        } else if ("@&".includes(c)) {
+            tokens.push({
+                type: "mod",
+                content: c
             })
         } else if (opTypes.any(c)) {
             let fullNam = c
@@ -169,11 +239,17 @@ function tokenize(program) {
                 c = program[++currentChar]
             }
             currentChar--
-            if (fullNum == ".") fullNum = "0"
-            tokens.push({
-                type: "num",
-                content: fullNum
-            })
+            if (fullNum == ".") {
+                tokens.push({
+                    type: "opr",
+                    content: "."
+                })
+            } else {
+                tokens.push({
+                    type: "num",
+                    content: fullNum
+                })
+            }
         } else if (c == '"') {
             let fullString = ""
             c = program[++currentChar]
@@ -197,7 +273,7 @@ function tokenize(program) {
         } else if ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_".any(c)) {
             let fullNam = c
             c = program[++currentChar]
-            while ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_".any(c)) {
+            while ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789_".any(c)) {
                 fullNam += c
                 c = program[++currentChar]
             }
@@ -277,6 +353,10 @@ function parse(tokens) {
             c++
             return {type: token.type}
         }
+        if (token.type == "mod") {
+            c++
+            return {type: token.type, content: token.content}
+        }
         error(`Unknown token: ${token.type} \`${token.content}\``)
     }
     while (c < tokens.length) {
@@ -290,7 +370,7 @@ function modify(ast) {
     let c = 0
     while (c < modified.length) {
         let p = modified[c++]
-        if (p.type == "paren") {
+        if (["paren", "arr"].includes(p.type)) {
             p.content = p.content.map(e => modify({content: e}).content)
         } else if (p.type == "block") {
             p = modify(p)
@@ -319,7 +399,7 @@ function operations(ast) {
     let c = 0
     while (c < modified.length) {
         let p = modified[c++]
-        if (p.type == "paren") {
+        if (["paren", "arr"].includes(p.type)) {
             p.content = p.content.map(e => operations({content: e}).content)
         } else if (p.type == "block") {
             p = operations(p)
@@ -362,7 +442,7 @@ function variables(ast) {
     let c = 0
     while (c < declaredVars.length) {
         let p = declaredVars[c++]
-        if (p.type == "paren") {
+        if (["paren", "arr"].includes(p.type)) {
             p.content = p.content.map(e => variables({content: e}).content)
         } else if (p.type == "block") {
             p = variables(p)
@@ -371,13 +451,28 @@ function variables(ast) {
         } else if (p.type == "ctrl") {
             p.arguments = p.arguments.map(e => variables({content: e}).content)
         } else if (formattedVarTypes.includes(p.type)) {
+            if (declaredVars[c].type == "call") {
+                p.content = {
+                    type: p.type,
+                    name: declaredVars[c].content,
+                    arguments: declaredVars[c].arguments.map(e => variables({content: e}).content)
+                }
+                p.type = "function"
+                declaredVars.splice(c, 1)
+                continue
+            } else if (declaredVars[c].type == "mod") {
+                p.array = true
+                declaredVars.splice(c, 1)
+            } else if (declaredVars[c].type != "nam") {
+                error(`Expected variable name after \`${p.content}\`, got \`${declaredVars[c].type}\``)
+            }
             // Declaration
             if (c >= declaredVars.length) error(`Expected variable name after \`${p.content}\``)
             if (c > declaredVars.length - 2 || declaredVars[c + 1].content != '=') {
                 p.content = {
                     type: p.type,
                     name: declaredVars[c].content,
-                    content: [ defaultContent[p.type.replace("Type",'')] ]
+                    // content: [ defaultContent[p.type.replace("Type",'')] ]
                 }
                 p.type = "declare"
                 declaredVars.splice(c, 1)
@@ -412,14 +507,18 @@ function control(ast) {
     let c = 0
     while (c < modified.length) {
         let p = modified[c++]
-        if (p.type == "paren") {
-            p.content = p.content.map(e => operations({content: e}).content)
+        if (["paren", "arr"].includes(p.type)) {
+            p.content = p.content.map(e => control({content: e}).content)
         } else if (p.type == "block") {
-            p = operations(p)
+            p = control(p)
         } else if (p.type == "call") {
-            p.arguments = p.arguments.map(e => operations({content: e}).content)
+            p.arguments = p.arguments.map(e => control({content: e}).content)
+        } else if (p.type == "declare") { // This could mess things up!
+            if (p.content.content) {
+                p.content.content = p.content.content.map(e => control({content: e}).content)
+            }
         } else if (p.type == "ctrl") {
-            p.arguments = p.arguments.map(e => operations({content: e}).content)
+            p.arguments = p.arguments.map(e => control({content: e}).content)
             p.name = p.content
             if (modified[c].type == "block") {
                 p.content = modified[c].content
@@ -427,12 +526,32 @@ function control(ast) {
                 p.content = [ modified[c] ]
             }
             modified.splice(c, 1)
+        } else if (p.type == "function") {
+            let type = modified[c].type
+            if (type == "block") {
+                control(modified[c])
+                //modified[c].content = modified[c].content.map(e => control({content: e}).content)
+                //console.log(modified[c].content)
+                p.content.content = modified[c].content
+                modified.splice(c, 1)
+            } else if (type == "call") {
+                p.content.content = [ modified[c] ]
+                modified.splice(c, 1)
+            } else {
+                error(`Unknown branch type \`${type}\``)
+            }
+        } else if (formattedVarTypes.includes(p.type)) {
+            p.type = "nam"
         }
     }
     return ast
 }
 
-function generate(node) {
+function addSemicolon(s) {
+    return (s[s.length - 1] == "}" ? s : s + ';')
+}
+
+function generate(node, parentType="") {
     let code = ""
 
     if (Array.isArray(node)) {
@@ -441,20 +560,29 @@ function generate(node) {
 
     switch (node.type) {
         case "Program":
-            return node.content.map(generate).map(e => (e[e.length - 1] == "}" ? e : e + ';'))
+            return node.content.map(e => generate(e, "Program")).map(addSemicolon)
                 .join("\n")
         case "block":
-            return "{\n" + node.content.map(generate).map(e => e + ';').join("\n") + "\n}"
+            return "{\n" + node.content.map(generate).map(addSemicolon).join("\n") + "\n}"
         case "call":
             if (node.content == stats["print"] && node.arguments.length > 1) {
                 let ret = node.arguments.map(e => `${stats["print"]}(${generate(e)})`)
-                return ret.join("; _(\" \"); ")
+                return ret.join(`; ${stats["print"]}(\" \"); `)
             }
             return node.content + "("
                 + node.arguments.map(generate).join(", ")
                 + ")"
+        case "function": // Not implemented!
+            let args = node.content.arguments.map(generate).join(", ")
+            let code = node.content.content.map(generate).map(addSemicolon)
+            let type = node.content.type.replace("Type", '')
+            if (type != "def")
+                code[code.length - 1] = `return ${code[code.length - 1]}`
+            functions += `${lTypesToCTypes[type]} ${node.content.name}(${args}) {\n`
+                + code.join('\n')
+                + "\n}\n"
+            return `// function \`${node.content.name}\``
         case "ctrl":
-            console.log(node.content)
             if (node.name == "for") {
                 let args = node.arguments.map(generate)
                 if (args.length == 1) {
@@ -467,44 +595,78 @@ function generate(node) {
                     if (args.length == 2) {
                         args = [
                             args[0],
-                            nam + " < " + args[1],
+                            args[1],
                             nam + "++"
                         ]
                     } else if (args.length == 3) {
                         args = [
                             args[0],
-                            nam + " < " + args[1],
+                            args[1],
                             nam + " += " + args[2]
                         ]
                     }
                 }
                 return node.name + " ("
                     + args.join("; ") + ") {\n"
-                    + node.content.map(generate).map(e => e + ';').join("\n")
+                    + node.content.map(generate).map(addSemicolon).join("\n")
                     + "\n}"
             } else if (node.arguments.length != 1) {
                 error(`Didn't expect ${node.arguments.length} arguments for \`${node.name}\``)
             }
+            console.log(node)
+            if (node.content == undefined) {
+                error("For some reason the if statement didn't get a block? Idk.")
+            }
             return node.name + " ("
                 + node.arguments.map(generate).join(", ") + ") {\n"
-                + node.content.map(generate).map(e => e + ';').join("\n")
+                + node.content.map(generate).map(addSemicolon).join("\n")
                 + "\n}"
         case "num":
             return node.content
         case "str":
-            return 'std::string("' + node.content + '")'
+            return 'string("' + node.content + '")'
         case "opr":
-            //return `(${generate(node.arguments[0])} ${node.name} ${generate(node.arguments[1])})`
-            if (operationFunctions[node.name] == undefined) error(`Unknown operation \`${JSON.stringify(node)}\``)
-            return `_${operationFunctions[node.name]}(${generate(node.arguments[0])}, ${generate(node.arguments[1])})`
+            if (node.name == '.') {
+                //return `${generate(node.arguments[0])}.${operationFunctions[generate(node.arguments[1])]}()`
+                return operationFunctions[generate(node.arguments[1])].replace("%", generate(node.arguments[0]))
+            } else {
+                if (operationFunctions[node.name] == undefined) error(`Unknown operation \`${JSON.stringify(node)}\``)
+                return `_${operationFunctions[node.name]}(${generate(node.arguments[0])}, ${generate(node.arguments[1])})`
+            }
         case "arr": // Needs code here!
-            break
+            let inferredType = node.content[0][0].type
+            if (inferredType == "str" && lastVariableType != "str") {
+                error(`Can't assign variable type \`str\` to type \`${lastVariableType}\``)
+            } else if (inferredType == "num" && lastVariableType == "str") {
+                error(`Can't assign variable type \`num\` to type \`str\``)
+            }
+            let castType = ""
+            if (lastVariableType == "int") {
+                castType = "(int)"
+            }
+            return `vector<${lTypesToCTypes[lastVariableType]}>{` + node.content.map(e => castType + generate(e)).join(", ") + "} "
         case "paren":
             return `(${node.content.map(generate).join(" ")})`
         case "nam":
             return node.content
         case "declare":
-            return `${lTypesToCTypes[node.content.type.replace('Type','')]} ${node.content.name} = ${generate(node.content.content)}`
+            lastVariableType = node.content.type.replace('Type','')
+            let varType = lTypesToCTypes[lastVariableType]
+            if (node.array) {
+                varType = `vector<${varType}>`
+            }
+            let ret
+            if (node.content.content == undefined) {
+                ret = `${varType} ${node.content.name}`
+            } else {
+                ret = `${varType} ${node.content.name} = ${generate(node.content.content)}`
+            }
+            if (parentType == "Program") {
+                variableCode += ret + ';\n'
+                return `// variable \`${node.content.name}\``
+            } else {
+                return ret
+            }
         case "modify":
             return `${node.content.name} ${node.content.type} ${generate(node.content.content)}`
         default:
--- Lang/test.cpp
+++ Lang/test.cpp
@@ -1,27 +1,95 @@
 
+// std::vector<int> v { 10, 20, 30 };
+
+// transform(v.begin(), v.end(), v.begin(), [](int &c){ return c * 2; });
+
+// for (int a : v) {
+//     print(a); print(" ");
+// }
+
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
 
-std::string _(std::string i) { std::cout << i; return i; }
-float       _(float i)       { std::cout << i; return i; }
+using namespace std;
 
-float       _sum(float a      , float b      ) { return a + b; }
-std::string _sum(std::string a, float b      ) { return a + std::to_string(b); }
-std::string _sum(float a      , std::string b) { return std::to_string(a) + b; }
-std::string _sum(std::string a, std::string b) { return a + b; }
+class String {
+    public:
+        static int toint(string a) {
+            try {
+                return stoi(a);
+            } catch (...) {
+                return 0;
+            }
+        }
 
-float       _mul(float a      , float b      ) { return a * b; }
-std::string _mul(std::string a, int b        ) { std::string r = ""; for (int c = 0; c < b; c++) { r += a; } return r; }
+        static string lower(string a) {
+            transform(a.begin(), a.end(), a.begin(), [](unsigned char c){ return tolower(c); });
+            return a;
+        }
 
-int         _eql(float a      , float b      ) { return a == b; }
-int         _eql(std::string a, std::string b) { return a == b; }
-int         _lss(float a      , float b      ) { return a < b; }
-int         _lss(std::string a, std::string b) { return a.length() < b.length(); }
-int         _mrr(float a      , float b      ) { return a > b; }
-int         _mrr(std::string a, std::string b) { return a.length() > b.length(); }
+        static string upper(string a) {
+            transform(a.begin(), a.end(), a.begin(), [](unsigned char c){ return toupper(c); });
+            return a;
+        }
+
+        static int length(string a) {
+            return a.length();
+        }
+};
+
+class Vector {
+};
+
+string print(string i) { cout << i; return i; }
+float  print(float i)  { cout << i; return i; }
+
+string input(string i) { cout << i; string _retstr; getline(cin, _retstr); return _retstr; }
+
+float  _sum(float a , float b ) { return a + b; }
+string _sum(string a, float b ) { return to_string(b) + a; }
+string _sum(float a , string b) { return to_string(a) + b; }
+string _sum(string a, string b) { return a + b; }
+
+float  _sub(float a , float b ) { return a - b; }
+// string _sub(string a, float b ) { return to_string(b) + a; }
+// string _sub(float a , string b) { return to_string(a) + b; }
+// string _sub(string a, string b) { return a + b; }
+
+float  _mul(float a , float b ) { return a * b; }
+string _mul(string a, int b   ) { string r = ""; for (int c = 0; c < b; c++) { r += a; } return r; }
+
+int _eql(float a , float b ) { return a == b; }
+int _eql(string a, string b) { return a == b; }
+
+int _lss(float a , float b ) { return a < b; }
+int _lss(string a, string b) { return a.length() < b.length(); }
+
+int _leq(float a , float b ) { return a <= b; }
+int _meq(float a , float b ) { return a >= b; }
+
+int _mrr(float a , float b ) { return a > b; }
+int _mrr(string a, string b) { return a.length() > b.length(); }
+
+int number = String::toint(input(string("Enter the number of terms: ")));
+int n1 = 0;
+int n2 = 1;
+int nextTerm;
 
 int main() {
-    _(_lss(std::string("a"), std::string("heyy")));
+    // variable `number`;
+    // variable `n1`;
+    // variable `n2`;
+    // variable `nextTerm`;
+    print(string("\nFibonacci: \n"));
+    for (int a = 0; _lss(a, number); a++) {
+        print(n1); print(" "); print(string(""));
+        nextTerm = _sum(n1, n2);
+        n1 = n2;
+        n2 = nextTerm;
+    }
     std::cout << '\n';
     return 0;
 }
