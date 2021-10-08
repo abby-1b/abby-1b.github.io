eea731bdc37a4da50ab26eaee2bda53ea4ec93b8
CodeIGuess <codeiguess@gmail.com>
2021-10-07 20:30:18 -0400

    Crouching

--- Engine/script.js
+++ Engine/script.js
@@ -1,6 +1,10 @@
 
 let con = new Console(300, 200, "#ebe3c5")
 
+// Text
+con.nFont("rh", "rainyhearts.ttf")
+con.font("rh")
+
 // Controls
 con.nEvent("jump", () => {
     if (player.onGround)
@@ -22,47 +26,25 @@ plant.addAnimation("default", {
 
 // Player
 let player = con.nObj(new PhysicsActor("Sprites/Player.png", 0, 0, 16, 16, false))
-player.addAnimation("idle", {
-    start: 0,
-    end: 5,
-    timer: 6,
-    loop: true,
-    pause: -1
-}, true)
-player.addAnimation("run", {
-    start: 6,
-    end: 11,
-    timer: 3,
-    loop: true,
-    pause: -1
-}, true)
-player.addAnimation("jump", {
-    start: 12,
-    end: 13,
-    timer: 12,
-    loop: false,
-    pause: -1
-})
-player.addAnimation("fall", {
-    start: 14,
-    end: 16,
-    timer: 8,
-    loop: false,
-    pause: -1
-})
+player.addAnimation("idle", { start: 0 , end: 5 , timer: 6 , loop: true , pause: -1 }, true)
+player.addAnimation("run" , { start: 6 , end: 11, timer: 3 , loop: true , pause: -1 })
+player.addAnimation("jump", { start: 12, end: 13, timer: 12, loop: false, pause: -1 })
+player.addAnimation("fall", { start: 14, end: 16, timer: 8 , loop: false, pause: -1 })
+player.addAnimation("down", { start: 17, end: 18, timer: 3 , loop: false, pause: -1 })
+player.addAnimation("cmov", { start: 18, end: 19, timer: 5 , loop: true, pause: -1 })
 
 player.flipped = true
-// player.showHitbox = true
+player.showHitbox = true
 player.isCrouched = false
 player.hbOffsets({top: 3, bottom: 0, left: 5, right: 6})
-player.pos.x += 775
+player.pos.x += 700 // 75
 
 player.onCollision(function(el, d) {
     if (el.srcStr == "Tiles/Bounce.png" && d == "top") {
         player.speed.y = -2.3
         player.animate("jump")
-    } else if (el.srcStr == "Sprites/Trash.png" && d == "top" && (el.curAnimationTimer == 0 || el.curAnimationTimer == 120)) {
-        player.speed.y -= 11
+    } else if (el.srcStr == "Sprites/Trash.png" && d == "top") {
+        player.speed.y = -1.5
     }
 })
 
@@ -148,26 +130,16 @@ let gameLoop = () => {
         player.speed.add(new Vec2(
             ('d' in con.keys ? 1 : 0) - ('a' in con.keys ? 1 : 0), 0).normalized().mulr(0.25))
     
-    // if ('s' in con.keys && player.onGround) {
-    //     if (player.animationStart != 17) {
-    //         player.animationStart = 17
-    //         player.curAnimationTimer = 4
-    //         player.animationFrame = 0
-    //         player.animationFrames = 2
-    //         player.animationTimer = 4
-    //         player.loopAnimation = false
-    //     }
-    //     player.isCrouched = true
-    //     player.hbOffsets({top: 10, bottom: 0, left: 5, right: 5})
-    //     player.speed.x *= 0.7
-    // } else {
-    //     if (player.canUnCrouch) {
-    //         player.hbOffsets({top: 3, bottom: 0, left: 6, right: 6})
-    //         player.isCrouched = false
-    //     }
-    //     if (!player.canUnCrouch) player.speed.x *= 0.7
-    //     else player.speed.x *= 0.87
-    // }
+    if ('s' in con.keys && player.onGround) {
+        player.isCrouched = true
+        player.speed.x *= 0.7
+    } else {
+        // if (player.canUnCrouch) {
+            player.isCrouched = false
+        // }
+        // if (!player.canUnCrouch) player.speed.x *= 0.7
+        // else player.speed.x *= 0.87
+    }
     // if (ts.length > 1 && ' ' in con.keys) {
     //     if (player.onGround && player.canUnCrouch) {
     //         player.isCrouched = false
@@ -202,21 +174,28 @@ let gameLoop = () => {
     //     player.curAnimationTimer = player.animationTimer
     // }
     if (player.onGround) {
-        if (Math.abs(player.speed.x) > 0.1)
-            player.animate("run")
-        else
-            player.animate("idle")
+        if (player.isCrouched) {
+            if (Math.abs(player.speed.x) > 0.1)
+                player.animate("cmov")
+            else
+                player.animate("down")
+            
+            player.hbOffsets({top: 10, bottom: 0, left: 3, right: 5})
+        } else {
+            if (Math.abs(player.speed.x) > 0.1)
+                player.animate("run")
+            else
+                player.animate("idle")
+
+            player.hbOffsets({top: 3, bottom: 0, left: 5, right: 6})
+        }
     } else {
         if (player.speed.y < 0) {
             player.animate("jump")
         } else {
             player.animate("fall")
         }
+        player.hbOffsets({top: 3, bottom: 0, left: 5, right: 6})
     }
     if (player.speed.x != 0) player.flipped = player.speed.x < 0
-    if (player.flipped)
-        player.hbOffsets({top: 3, bottom: 0, left: 6, right: 5})
-    else
-        player.hbOffsets({top: 3, bottom: 0, left: 5, right: 6})
-    // player.canUnCrouch = true
 }
