309e57a5171aa07a943045bcc7abe79124c0f3a5
CodeIGuess <codeiguess@gmail.com>
2021-10-14 10:11:44 -0400

    Everything.

--- mdc.js
+++ mdc.js
@@ -13,31 +13,48 @@ function mdToHtml(t) {
 		"?": "question"
 	}
 	let m = ""
+	// Metadata
 	t = t.replace(/\{(\n|.)*?\}\n{1,}/gm, e => { m = JSON.parse(e); return "" })
+	// Headers (#)
 		.replace(/^(\s*)(#{1,6} )(?=.*$)/gm, e => "<h" + e.trim().length + ">")
 		.replace(/^<h[0-9]>.*/gm, e => e + "</" + e.split(">")[0].slice(1) + ">")
 		
+	// Bullet lists
 		.replace(/^\s*?~(.|\n)*?\n\n/gm, e => "<ul>\n" + e.trim() + "\n</ul>\n\n")
 		.replace(/^(\s*?)~(\s|)/gm, "<li>")
 
+	// Checklists
 		.replace(/^\s*?\[.\](.|\n)*?\n\n/gm, e => "<ul>\n" + e.trim() + "\n</ul>\n\n")
 		.replace(/^(\s*?)\[.\](\s|)/gm, e => "<li class='" + cmIconToClass[e.trim()[1]] + "'>")
 		.replace(/^<li >.*/gm, e => e + "</li>")
 
+	// Add / Remove lists
 		.replace(/^\s*?[+|-](.|\n)*?\n\n/gm, e => "<ul>\n" + e.trim() + "\n</ul>\n\n")
 		.replace(/^(\s*?)[+|-](\s|)/gm, e => e.trim() == "+" ? "<li class='added'>" : "<li class='removed'>")
 
+	// Closes all list items
 		.replace(/^<li.*/gm, e => e + "</li>")
+	
+	// Links
+		.replace(/\[.*?]\(.*?\)/gm, e => "<a onclick=\"openURL('" + e.split("](")[1].slice(0,-1) + "')\">" + e.split("](")[0].slice(1) + "</a>")
 
+	// Bold, italics, combinations of <
 		.replace(/\*\*[^*]*?\*\*/gm, e => "<b>" + e.slice(2, -2) + "</b>")
 		.replace(/\*[^*]*?\*/gm, e => "<i>" + e.slice(1, -1) + "</i>")
 		.replace(/_.*?_/gm, e => "<i>" + e.slice(1, -1) + "</i>")
+	
+	// Monospace
 		.replace(/(?<!\`)`[^`]{1,}`/gm, e => "<m>" + e.slice(1, -1) + "</m>")
 		.replace(/```.*$/gm, "```")
 		.replace(/```(.|\n)*?```/gm, e => "<div class='spacer'></div><m class='block'>" + e.slice(3, -3).trim().replace(/\n/g, "<div class='spacer'></div>") + "</m>")
+
+	// Superscript
 		.replace(/\^\d{1,}/g, e => "<sup>" + e.slice(1) + "</sup>")
+	
+	// Spacers (newlines, basically)
 		.replace(/\n\n/gm, "\n<div class='spacer'></div>")
 
+	// Removes all the unused things
 		.replace(/\n\</g, "<")
 		.replace(/>\n/g, ">")
 	return {text: t, meta: m}
--- multiPage.js
+++ multiPage.js
@@ -1,18 +1,28 @@
-
-let pageLocation = window.location.search
-if (pageLocation !== '') pageLocation = '/' + pageLocation.slice(1)
-
 invert()
+hud()
 
-fetch(pageLocation + "/site.mdc").then(r => r.text().then(text => {
-	text = mdToHtml(text)
-	document.body.children[0].innerHTML = text.text
-	document.title = text.meta.title
-	if (text.meta.navbar) {
-		
-	}
-	hud()
-}))
+function openPage(pageLocation) {
+	if (pageLocation !== '') pageLocation = '/' + pageLocation.slice(1)
+	fetch(pageLocation + "/site.mdc").then(r => r.text().then(text => {
+		text = mdToHtml(text)
+		document.body.children[0].innerHTML = text.text
+		document.title = text.meta.title || "Code (I Guess)"
+		if (text.meta.navbar) {
+
+		}
+	}))
+}
+openPage(window.location.search)
+
+function openURL(url) {
+	openPage(url)
+	window.history.pushState('page2', document.title, url);
+}
+
+window.onpopstate = function(e) {
+	console.log(e)
+	openPage(window.location.search)
+}
 
 function hud() {
 	let darkModeButton = document.createElement("div")
@@ -50,10 +60,12 @@ function setColors(m) {
 		document.documentElement.style.setProperty("--fg", "255,255,255")
 		document.documentElement.style.setProperty("--green", "#dfd")
 		document.documentElement.style.setProperty("--red", "#fdd")
+		document.documentElement.style.setProperty("--accentSeeThrough", "0.8")
 	} else {
 		document.documentElement.style.setProperty("--bg", "255,255,255")
 		document.documentElement.style.setProperty("--fg", "0,0,0")
 		document.documentElement.style.setProperty("--green", "#050")
 		document.documentElement.style.setProperty("--red", "#500")
+		document.documentElement.style.setProperty("--accentSeeThrough", "0.6")
 	}
 }
