6f0483c04fb0a0ac6f80ef34f25d90b4b57ef7f5
CodeIGuess <codeiguess@gmail.com>
2021-10-29 23:06:04 -0400

    Names!

--- GameOfWords/guest/enter.js
+++ GameOfWords/guest/enter.js
@@ -1,44 +1,65 @@
 
-var codeContainer = document.getElementById("codeContainer")
+let codeContainer = document.getElementById("codeContainer")
+let nameContainer = document.getElementById("nameContainer")
 
 tPeer = new Peer()
 let conn = -1
 
 let keypressListener = function(e) {
-	if (e.key == "Enter" && this.value.length == 3) {
-		codeContainer.removeEventListener('keypress', keypressListener)
-		tCode = this.value
-		connectToCode()
-	}
+	if (this == codeContainer) {
+		if (e.key == "Enter" && this.value.length == 3) {
+			this.removeEventListener('keypress', keypressListener)
+			tCode = this.value
+			connectToCode()
+		}
 
-	if (e.ctrlKey || e.metaKey || e.altKey) return
-	if ("qwrtyuiopsghjklzxvnm".includes(e.key.toLowerCase())
-		|| (e.key.length == 1 && codeContainer.value.length >= 3
-		&& Math.abs(e.target.selectionEnd - e.target.selectionStart) == 0)) {
-		e.preventDefault()
-		return
-	}
+		if (e.ctrlKey || e.metaKey || e.altKey) return
+		if ("qwrtyuiopsghjklzxvnm".includes(e.key.toLowerCase())
+			|| (e.key.length == 1 && this.value.length >= 3
+			&& Math.abs(e.target.selectionEnd - e.target.selectionStart) == 0)) {
+			e.preventDefault()
+			return
+		}
 
-	if ((e.key.charCodeAt(0) >= 97) && (e.key.charCodeAt(0) <= 122)) {
-		let start = e.target.selectionStart
-		e.target.value = e.target.value.substring(0, start)
-			+ String.fromCharCode(e.key.charCodeAt(0) - 32)
-			+ e.target.value.substring(e.target.selectionEnd)
-		e.target.setSelectionRange(start + 1, start + 1)
-		e.preventDefault()
+		if ((e.key.charCodeAt(0) >= 97) && (e.key.charCodeAt(0) <= 122)) {
+			let start = e.target.selectionStart
+			e.target.value = e.target.value.substring(0, start)
+				+ String.fromCharCode(e.key.charCodeAt(0) - 32)
+				+ e.target.value.substring(e.target.selectionEnd)
+			e.target.setSelectionRange(start + 1, start + 1)
+			e.preventDefault()
+		}
+	} else if (this == nameContainer) {
+		if (e.key == "Enter") {
+			this.removeEventListener('keypress', keypressListener)
+			tPeer.on('connection', (conn) => {
+				conn.on('data', (data) => {
+					console.log("Received:", data)
+				})
+			})
+			conn.send("name:" + this.value)
+		}
 	}
 }
 codeContainer.addEventListener('keypress', keypressListener)
 
 function connectToCode() {
 	conn = tPeer.connect(codeToID(tCode))
+	log("Connecting to host...")
 	conn.on('open', function() {
-		console.log("Good! Next thing now.")
+		log("Connected to host.")
 		document.getElementById("codeFrame").classList.add("hidden")
 		document.getElementById("nameFrame").classList.remove("hidden")
+		nameContainer.addEventListener('keypress', keypressListener)
+	})
+	conn.on('data', (data) => {
+		console.log("Received:", data)
+		if (data.startsWith("name:")) {
+			if (data.split(':')[1] == '0') {
+				console.log("Name not available!")
+			} else {
+				console.log("Name registered!")
+			}
+		}
 	})
 }
-
-
-document.getElementById("codeFrame").classList.add("hidden")
-document.getElementById("nameFrame").classList.remove("hidden")
