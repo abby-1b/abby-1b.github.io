56004787338f9acf9bba204edc08adcbeaa559a3
CodeIGuess <codeiguess@gmail.com>
2021-11-03 15:43:27 -0400

    Testing autotiling

--- GameOfWords/game/game.js
+++ GameOfWords/game/game.js
@@ -1,24 +1,27 @@
 
 let con = new Console(200, "white")
 
-// let player = new PhysicsActor("../Art/test.png", 0, 0, 8, 8)
+// let player = new PhysicsActor("Art/test.png", 0, 0, 8, 8)
 // player = con.nObj(player)
 // player.bounce = 0.8
 
-let tileSet = new TileSet("../Art/TestSetNew.png", 8, 8)
-let tileMap = con.nObj(new TileMap(tileSet, 8, 8, [
-   1, 1, 0, 0, 0, 0, 0, 0,
-   1, 1, 0, 0, 1, 0, 0, 0,
-   1, 0, 0, 1, 1, 0, 0, 0,
-   0, 0, 0, 1, 1, 1, 1, 0,
-   0, 0, 1, 0, 0, 0, 0, 0,
-   0, 0, 0, 0, 0, 1, 0, 0,
-   0, 0, 1, 0, 0, 1, 0, 1,
-   0, 0, 0, 0, 0, 1, 0, 1
-]))
+let tileSet = new TileSet("Art/TestSetNew.png", 8, 8)
+let tileMap = con.nObj(TileMap.from("Maps/Map1.png", tileSet, {
+	TILE: [197, 204, 184]
+}))
 
 con.init(() => {
 
 })
 
 con.loop(() => {})
+
+// This is some autotiling stuff. Ignore it.
+// const UP = 1
+// const DOWN = 2
+// const LEFT = 4
+// const RIGHT = 8
+// const ULEFT = 16
+// const URIGHT = 32
+// const DLEFT = 64
+// const DRIGHT = 128
