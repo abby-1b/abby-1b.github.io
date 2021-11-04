f9a61f456e1773283fee7ca423a468e173e7af8e
CodeIGuess <codeiguess@gmail.com>
2021-11-04 09:35:22 -0400

    Further testing?

--- GameOfWords/game/game.js
+++ GameOfWords/game/game.js
@@ -1,9 +1,10 @@
 
 let con = new Console(200, "white")
 
-// let player = new PhysicsActor("Art/test.png", 0, 0, 8, 8)
-// player = con.nObj(player)
-// player.bounce = 0.8
+let player = con.nObj(new PhysicsActor("Art/test.png", 0, 0, 8, 8))
+player.bounce = 0.8
+
+con.follow(player, 0.1, new Vec2(0, 0))
 
 let tileSet = new TileSet("Art/TestSetNew.png", 8, 8)
 let tileMap = con.nObj(TileMap.from("Maps/Map1.png", tileSet, {
@@ -14,7 +15,9 @@ con.init(() => {
 
 })
 
-con.loop(() => {})
+con.loop(() => {
+	
+})
 
 // This is some autotiling stuff. Ignore it.
 // const UP = 1
