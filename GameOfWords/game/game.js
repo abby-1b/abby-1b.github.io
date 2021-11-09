9976849f3d4bdf067c044fc8aa8f753c6d2f91b3
CodeIGuess <codeiguess@gmail.com>
2021-11-08 21:33:11 -0400

    Vibing around with the player

--- GameOfWords/game/game.js
+++ GameOfWords/game/game.js
@@ -5,32 +5,34 @@ let tileSet = new TileSet("Art/TestSetNew.png", 8, 8)
 let tileMap = con.nObj(TileMap.from("Maps/Map1.png", tileSet, {
 	TILE: [197, 204, 184]
 }))
-tileMap.hbOffsets({top: 3, bottom: 0, left: 0, right: 0})
+tileMap.hbOffsets({top: 4, bottom: 0, left: 0, right: 0})
 
-let player = con.nObj(new PhysicsActor("Art/test.png", 0, 0, 8, 8))
-con.follow(player, 0.1, new Vec2(30, 30))
+let player = con.nObj(new PhysicsActor("Art/player.png", 0, 0, 8, 10))
+// con.follow(player, 0.1, new Vec2(30, 30))
+con.follow(player, 0.1, new Vec2(0, 0))
 // player.bounce = 1
 
 con.nEvent("left", () => {})
 con.nEvent("right", () => {})
 con.nEvent("jump", () => {
-	if (player.onGround) {
+	// if (player.onGround) {
 		player.speed.y = -1
-	}
+	// }
 })
 Controllers.new(player, "platformer")
 
 player.onCollision(function(el, d) {
 	if (d == "top" && player.bounce != 0) {
 		console.log(player.speed.y)
-		player.speed.y = -1.5
+		// player.speed.y = -1.5 * player.bounce
 	}
 })
 
 con.init(() => {})
 
 con.loop(() => {
-	con.text(CTool.round(Math.abs(player.speed.y), 2), 1, 1)
+	con.text(CTool.round(con.frameRate, 2), 1, 1)
+	// con.text(player.speed.y, 1, 1)
 
 	if (!player.locked)
         player.speed.addVec(new Vec2(
