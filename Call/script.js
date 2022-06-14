9f48f8179fcca5e810adcd4d8a05d73233385df3
Code (I Guess) <46802090+CodeIGuess@users.noreply.github.com>
2022-06-13 22:23:28 -0400

    Update script.js

--- Call/script.js
+++ Call/script.js
@@ -1,7 +1,7 @@
 var playing = true
 let audio = new Audio("audio.mp3")
 let names = "Fabi,Platonic Soulmate,darling,Mari,My Dear,sweetheart <3".split(",")
-function play() {
+function play(kk) {
 	playing = !playing
 	console.log(playing)
 	if (playing) {
@@ -10,11 +10,11 @@ function play() {
 		        document.body.style.opacity = '1'
                 }, (5 + Math.random() * 10) * 1000)
 	} else {
-                playing = true
+                if (!kk) playing = true
 		audio.pause()
 		audio.currentTime = 0
 		document.body.style.opacity = '0'
 		document.querySelector("a").innerText = names[Math.round(Math.random() * 1000) % names.length]
 	}
 }
-play()
+play(true)
